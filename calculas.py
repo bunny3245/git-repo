270b90511994804303e93d9b2176be11e1cba46e
bunny3245 <nk780877@gmail.com>
2025-07-08 11:19:47 +0500

    Update calculas.py

--- calculas.py
+++ calculas.py
@@ -7,7 +7,7 @@ class new_class:
 print("hello version1 ")
 print("hello version2 ")
 print("hello version3 ")
-jahdfhhkhhdfhakh
+jahdfhhkhhdfhakh4646373
 object1 = new_class('shazi', "shaziii")
 object2 = new_class('bunny', 'bunn')
 print(object1.name)
